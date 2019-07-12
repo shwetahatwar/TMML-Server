f83e680d8c09b0f4841e40346a17fe3ef2fc58ca
nikhil.kamble <nikhil.kamble@briot.in>
2019-07-12 11:51:36 +0530

    Barcode Logic

---
+++ api/models/BarcodeLogic.js
@@ -0,0 +1,20 @@
+/**
+ * BarcodeLogic.js
+ *
+ * @description :: A model definition represents a database table/collection.
+ * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+    barcodeType:{
+      type:'String'
+    },
+    barcodeSerial:{
+      type:'String'
+    },
+  },
+
+};
+
